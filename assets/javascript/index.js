dc7f3ced03f46540380b8c3ff692d7e88460c5ec
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2020-04-30 22:33:37 -0600

    javascript-click-and-add

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,3 +2,15 @@ open = () => {
     console.log( 'hello' )
 }
 open()
+
+pictureChoice = () => {
+    $( '.picture' ).on( "click", function () {
+        var points = Math.floor( ( Math.random() ) * 12 )
+        console.log( points )
+        total = points++
+        console.log( total )
+        console.log( 'You clicked a picture' )
+    } )
+}
+
+pictureChoice()
